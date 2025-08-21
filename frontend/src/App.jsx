58659088e08c4cf2ae57ddfdfd9cc8b296dd32ce
chetram <patelchetram49@gmail.com>
2025-08-21 11:24:02 +0530

    chore: setting up redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import Navbar from "./components/Navbar"
 
+import Login from "./pages/Login"
+
 import MainRoutes from "./routes/MainRoutes"
 
 const App = () => {
+
+
   return (
     <div>
       <Navbar />
       <MainRoutes />
+      {/* <Login/> */}
+       
     </div>
   )
 }
